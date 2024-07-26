60525e5c42122531dc3539003576d7b36288f020
Katya <92756386+kawatash1@users.noreply.github.com>
2024-07-26 17:36:23 +0300

    Update Virtual_Assistant.py

--- Virtual_Assistant.py
+++ Virtual_Assistant.py
@@ -1,1 +1,29 @@
+import webbrowser as wb
+import tkinter as tk
+from Web_Search import Websearch
 
+
+class App():
+	root = tk.Tk()				
+
+
+	root.title("Виртуальный дворецкий")    
+	root.geometry("700x500")				
+	root.config(bg="yellow")
+	
+	
+	L1 = tk.Label(text="Hello, I'm Virtual assistant", 
+				  font='Arial 30', fg="Blue", bg="yellow")
+	L1.pack()							
+
+
+	b1 = tk.Button(text="GOOGLE", command = Websearch.open_chrome)
+	b1.pack(pady = 20)
+	
+	b2 = tk.Button(text="Close", command = root.destroy)
+	b2.pack(pady = 20)
+
+	editor = tk.Text(height=10, wrap = "char")
+	editor.pack()
+
+	root.mainloop()
