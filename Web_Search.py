f8c1ef5a47e2e59be725d3c0eaf95b965b693c64
Katya <92756386+kawatash1@users.noreply.github.com>
2024-07-26 17:36:42 +0300

    Update Web_Search.py

--- Web_Search.py
+++ Web_Search.py
@@ -1,1 +1,17 @@
+import webbrowser as wb
+import string
+#from Virtual_Assistant import 
+
+class Websearch():
+    
+    def open_chrome():
+        wb.open_new("https://www.google.com/")
+
+    def SearchChrome(s):
+        newS = s.replace(' ', '+')
+        q = "https://www.google.com/search?q=" + newS + '&'
+        wb.open_new(q)
+
+   
+
 
